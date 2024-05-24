f125ba57af51f186fb8e25db234a70bb04fc2fd5
scottg <scottghome@scottgs-MBP.lan>
2024-05-24 09:12:36 -0500

    reverse a linked list

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,34 +1,55 @@
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
 class LinkedList:
-    
+    def __init__(self):
+        self.head = None
 
-class SolveReverseLinkedList:
-    def reverse_list(self, head: ListNode) -> ListNode:
-        prev, curr = None, head
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
 
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
+    def reverse_list(self):
+        prev_node = None
+        current_node = self.head
+        while current_node is not None:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        self.head = prev_node
 
-        return prev
-    
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
 
-n1 = ListNode()
-n1.val = 1
-n2 = ListNode()
-n2.val = 2
-n3 = ListNode()
-n3.val = 3
-n4 = ListNode()
-n4.val = 4
-n5 = ListNode()
-n5.val = 5
+# Create a new linked list
+linked_list = LinkedList()
+
+# Append some nodes
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+
+print("Original List:")
+linked_list.print_list()  # Output: 1 2 3 4
+
+# Reverse the list
+linked_list.reverse_list()
+
+print("Reversed List:")
+linked_list.print_list()  # Output: 4 3 2 1
 
 
